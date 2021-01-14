e9ae484168f031c1fdb4aa2a7011907a618f5349
Crowdbotics <team@crowdbotics.com>
2021-01-14 09:08:28 +0000

    Initial commit.

---
+++ ios/lsp_stream_receivin_23840Tests/lsp_stream_receivin_23840Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface lsp_stream_receivin_23840 : XCTestCase
+
+@end
+
+@implementation lsp_stream_receivin_23840
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
